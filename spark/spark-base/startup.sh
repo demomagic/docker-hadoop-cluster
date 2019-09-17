c14361dbd76fe51e4d50ac4970de4641bc997881
demomagic <369587353@qq.com>
2019-09-17 22:01:33 +0800

    spark conf

--- spark/spark-base/startup.sh
+++ spark/spark-base/startup.sh
@@ -2,7 +2,7 @@
 
 hadoop fs -mkdir -p    /user/spark/jars
 hadoop fs -chmod g+w  /user/spark/jars
-hdfs fs -put $SPARK_HOME/jars/* /user/spark/jars
+hadoop fs -put $SPARK_HOME/jars/* /user/spark/jars
 
 cd $LIVY_HOME/bin
-./bin/livy-server
+./livy-server
